a54f74ce3d5b2faa99b89d9f01b48af4d5190ed3
Bryan Smith <mugenman1111@gmail.com>
2021-05-06 23:34:50 -0500

    Stage Repo for RunicSwords

---
+++ JotunnModStub/RuneSwords.cs
@@ -0,0 +1,53 @@
+﻿// JotunnModStub
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    JotunnModStub.cs
+// Project: JotunnModStub
+
+using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using Jotunn.Utils;
+
+namespace JotunnModStub
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Major)]
+    internal class RuneSwords : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.zarboz.runicswords";
+        public const string PluginName = "RuneSwords";
+        public const string PluginVersion = "0.0.1";
+
+        private void Awake()
+        {
+            
+        }
+
+        private void LoadAssets()
+        {
+
+        }
+
+        private void IceSword()
+        {
+
+        }
+
+        private void FireSword()
+        {
+
+        }
+
+        private void PoisonSword()
+        {
+
+        }
+
+        private void LightningSword()
+        {
+
+        }
+    }
+}
