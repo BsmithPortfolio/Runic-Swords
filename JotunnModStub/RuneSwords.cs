9202162154d8b104ad3e408efc3a21955fcec6e9
Bryan Smith <mugenman1111@gmail.com>
2021-05-07 11:39:36 -0500

    Update recipes

--- JotunnModStub/RuneSwords.cs
+++ JotunnModStub/RuneSwords.cs
@@ -18,7 +18,7 @@ namespace RuneSwords
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
-    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Major)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Major)]
     internal class RuneSwords : BaseUnityPlugin
     {
         public const string PluginGUID = "com.zarboz.runicswords";
@@ -49,12 +49,15 @@ namespace RuneSwords
                     Amount = 1,
                     Name = "Runic Ice Sword",
                     Enabled = true,
-                    CraftingStation = "forge",
-                    RepairStation = "forge",
+                    CraftingStation = "piece_artisanstation",
+                    RepairStation = "piece_artisanstation",
                     MinStationLevel = 4,
                     Requirements = new[]
                     {
-                        new RequirementConfig {Item = "Obsidian", Amount = 100}
+                        new RequirementConfig {Item = "Obsidian", Amount = 100},
+                        new RequirementConfig {Item = "Iron", Amount = 50, AmountPerLevel = 25},
+                        new RequirementConfig {Item = "FreezeGland", Amount = 25, AmountPerLevel = 10},
+                        new RequirementConfig {Item = "DragonTear", Amount = 10, AmountPerLevel = 5}
                     }
                 });
             ItemManager.Instance.AddItem(icerune);
@@ -70,12 +73,15 @@ namespace RuneSwords
                     Amount = 1,
                     Name = "Runic Fire Sword",
                     Enabled = true,
-                    CraftingStation = "forge",
-                    RepairStation = "forge",
+                    CraftingStation = "piece_artisanstation",
+                    RepairStation = "piece_artisanstation",
                     MinStationLevel = 4,
                     Requirements = new[]
                     {
-                        new RequirementConfig {Item = "Obsidian", Amount = 100}
+                        new RequirementConfig {Item = "Flametal", Amount = 100, AmountPerLevel = 50},
+                        new RequirementConfig {Item = "SurtlingCore", Amount = 75, AmountPerLevel = 35},
+                        new RequirementConfig {Item = "Iron", Amount = 50, AmountPerLevel = 25},
+                        new RequirementConfig {Item = "Obsidian", Amount = 50, AmountPerLevel = 25},
                     }
                 });
             ItemManager.Instance.AddItem(firerune);
@@ -90,12 +96,15 @@ namespace RuneSwords
                     Amount = 1,
                     Name = "Runic Poison Sword",
                     Enabled = true,
-                    CraftingStation = "forge",
-                    RepairStation = "forge",
+                    CraftingStation = "piece_artisanstation",
+                    RepairStation = "piece_artisanstation",
                     MinStationLevel = 4,
                     Requirements = new[]
                     {
-                        new RequirementConfig {Item = "Obsidian", Amount = 100}
+                        new RequirementConfig {Item = "Obsidian", Amount = 100, AmountPerLevel = 50},
+                        new RequirementConfig {Item = "Ooze", Amount = 50, AmountPerLevel = 25},
+                        new RequirementConfig {Item = "Iron", Amount = 50, AmountPerLevel = 25},
+                        new RequirementConfig {Item = "Guck", Amount = 25, AmountPerLevel = 10}
                     }
                 });
             ItemManager.Instance.AddItem(poison);
@@ -110,12 +119,15 @@ namespace RuneSwords
                     Amount = 1,
                     Name = "Runic Lightning Sword",
                     Enabled = true,
-                    CraftingStation = "forge",
-                    RepairStation = "forge",
+                    CraftingStation = "piece_artisanstation",
+                    RepairStation = "piece_artisanstation",
                     MinStationLevel = 4,
                     Requirements = new[]
                     {
-                        new RequirementConfig {Item = "Obsidian", Amount = 100}
+                        new RequirementConfig {Item = "Obsidian", Amount = 100, AmountPerLevel = 50},
+                        new RequirementConfig {Item = "Iron", Amount = 50, AmountPerLevel = 25},
+                        new RequirementConfig {Item = "TrophyEikthyr", Amount = 5, AmountPerLevel = 1},
+                        new RequirementConfig {Item = "HardAntler", Amount = 25, AmountPerLevel = 10}
                     }
                 });
             ItemManager.Instance.AddItem(llightrune);
